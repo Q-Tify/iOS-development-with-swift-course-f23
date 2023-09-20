3a5aa89d9ff4cd933fe671e1cdb00c7ead1bc394
Q-Tify <55044295+Q-Tify@users.noreply.github.com>
2023-09-20 05:02:01 +0300

    Add files via upload

--- hw1/1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
+++ hw1/1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/9. App Exercise - Ternary Messages.xcplaygroundpage/Contents.swift
@@ -8,11 +8,15 @@
 let stepGoal = 10000
 let steps = 3948
 
+print(steps < stepGoal / 2 ? "Almost halfway!" : "Over halfway!")
+
+/*
 if steps < stepGoal / 2 {
     print("Almost halfway!")
 } else {
     print("Over halfway!")
 }
+ */
 /*:
  _Copyright © 2021 Apple Inc._
 
--- hw1/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ hw1/2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -9,17 +9,35 @@ struct Book {
     var pages: Int
     var price: Double
     
+    func description() {
+        print("Title: \(title)")
+        print("Author: \(author)")
+        print("Pages: \(pages)")
+        print("Price: $\(price)")
+    }
 }
 
+let myBook = Book(title: "The Catcher in the Rye", author: "J.D. Salinger", pages: 224, price: 12.99)
 
+myBook.description()
 //:  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
 struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
-
+    
+    mutating func like() {
+        likes += 1
+    }
 }
 
+var myPost = Post(message: "My message!", likes: 0, numberOfComments: 0)
+
+print("Before: Likes = \(myPost.likes)")
+
+myPost.like()
+
+print("After: Likes = \(myPost.likes)")
 
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
